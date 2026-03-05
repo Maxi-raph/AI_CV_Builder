ff5d621afbb1425b9a7f0ff5f8ce8b47b1711e7c
Raph <onwujekwechibuzor@gmail.com>
2026-03-05 03:47:29 +0100

    done with sign up ui

--- app/Layouts/Auth.tsx
+++ app/Layouts/Auth.tsx
@@ -1,15 +1,17 @@
-import { Outlet } from 'react-router'
+import { Outlet, useLocation } from 'react-router'
 import { useSignUp } from '~/Context/signUpContext';
 
 
 const OnboardingLayout = () => {
     const {isSignedUp, setIsSignedUp} =useSignUp()
+    const location = useLocation()
+    if(location.pathname === 'Auth/login') setIsSignedUp(prev => true)
     return ( 
         <div className='bg-gray-300 p-6 h-screen'>
-            <section className='max-w-2xl mx-auto h-[420px] bg-white rounded-md relative z-10 grid grid-cols-2 gap-4'>
-                <div className={`flex flex-col pt-24 items-center gap-6 h-full w-[90%] bg-emerald-400 translate duration-500 p-6 
-                     ${isSignedUp ? 'translate-x-95 rounded-tr-md rounded-br-md' : 'translate-x-0 rounded-tl-md rounded-bl-md'}`}>
-                    <h2 className='text-white text-3xl font-bold'>Welcome Back!</h2>
+            <section className='max-w-3xl mx-auto  bg-white rounded-md relative z-10 grid grid-cols-2 gap-4'>
+                <div className={`flex flex-col relative z-10 pt-24 items-center gap-6 h-full w-[90%] bg-emerald-400 translate duration-500 p-4 
+                     ${isSignedUp ? 'translate-x-108 rounded-tr-md rounded-br-md' : 'translate-x-0 rounded-tl-md rounded-bl-md'}`}>
+                    <h2 className='text-white text-xl sm:text-2xl md:text-3xl font-bold'>Welcome Back!</h2>
                 </div>
                 <Outlet />
             </section>
--- app/routes/Auth/Sign_up/index.tsx
+++ app/routes/Auth/Sign_up/index.tsx
@@ -1,15 +1,58 @@
 import { useSignUp } from "~/Context/signUpContext";
+import { FaFacebookF ,FaLinkedin, FaGoogle, FaUser, FaEnvelope, FaLock } from "react-icons/fa";
 
 
 
 export default function SignUp() {
     const {isSignedUp, setIsSignedUp} =useSignUp()
   return (
-    <section className={`translate duration-500 p-6 ${isSignedUp?'-translate-x-80': '-translate-x-0'}`}>
-        <h2 className="text-emerald-400 font-bold text-3xl">Create Account</h2>
+    <section className={`translate duration-500 p-6 pt-10 relative z-8 ${isSignedUp?'-translate-x-93': '-translate-x-0'}`}>
+        <h2 className="text-emerald-400 font-bold text-center text-xl sm:text-2xl md:text-3xl">Create Account</h2>
+        <div className='flex w-full justify-center gap-4 mt-4 mb-4'>
+          <a href="#">
+            <div className="border border-gray-300 p-2 rounded-full">
+              <FaFacebookF className="text-lg" />
+            </div>
+          </a>
+          <a href="#">
+            <div className="border border-gray-300 p-2 rounded-full">
+              <FaGoogle className="text-lg"/>
+            </div>
+          </a>
+          <a href="#">
+            <div className="border border-gray-300 p-2 rounded-full">
+              <FaLinkedin className="text-lg" />
+            </div>
+          </a>
+        </div>
+        <p className="mb-4 text-gray-300 text-center">or use your email for registration</p>
        <form>
-        
-       </form>
+          <div className="relative mb-6">
+            <FaUser className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input 
+              type="text"
+              placeholder="Name"
+              className="pl-10 pr-3 py-2 bg-gray-100 focus:ring-2 focus:ring-emerald-300 outline-0 rounded-md w-full" 
+            />
+          </div>     
+           <div className="relative mb-6">
+            <FaEnvelope className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input 
+              type="email"
+              placeholder="Email"
+              className="pl-10 pr-3 py-2 bg-gray-100 focus:ring-2 focus:ring-emerald-300 outline-0 rounded-md w-full" 
+            />
+          </div>     
+           <div className="relative mb-6">
+            <FaLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input 
+              type="password"
+              placeholder="Password"
+              className="pl-10 pr-3 py-2 bg-gray-100 focus:ring-2 focus:ring-emerald-300 outline-0 rounded-md w-full" 
+            />
+          </div>   
+          <button type="submit" className="text-white bg-emerald-500 hover:bg-emerald-600 cursor-pointer px-12 py-2 rounded-3xl mx-auto flex">Sign Up</button>  
+        </form>
     </section>
   );
 }
