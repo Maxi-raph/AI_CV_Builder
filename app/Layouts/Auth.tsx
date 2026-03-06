c477eb46d3a9e5366ba1d752ac5258961f9505d9
Raph <onwujekwechibuzor@gmail.com>
2026-03-06 07:33:23 +0100

    i worked on the mobile responsiveness for the auth

--- app/Layouts/Auth.tsx
+++ app/Layouts/Auth.tsx
@@ -9,10 +9,10 @@ const OnboardingLayout = () => {
     const {transition, setTransition} = useTransition()
 
     return ( 
-        <div className='bg-gray-300 p-6 min-h-screen'>
-            <section className='max-w-3xl mx-auto  bg-white rounded-xl relative z-10 grid grid-cols-2 gap-4'>
-                <div className={`flex flex-col relative z-10 pt-24 items-center gap-5 h-full w-[90%] bg-emerald-500 translate duration-500 p-4 
-                     ${transition ? 'translate-x-[129%] rounded-tr-xl rounded-br-xl' : 'translate-x-[0%] rounded-tl-xl rounded-bl-xl'}`}>
+        <div className='bg-gray-300 pt-18 px-6 pb-8 min-h-screen'>
+            <section className='max-w-xl md:max-w-3xl mx-auto  bg-white rounded-xl relative z-10 grid  grid-cols-1 sm:grid-cols-2 sm:space-x-4'>
+                <div className={`hidden sm:flex flex-col relative z-10 pt-24 items-center gap-5 h-full w-[90%] bg-emerald-500 translate duration-500 p-4 
+                     ${transition ? 'translate-x-[131%] rounded-tr-xl rounded-br-xl' : 'translate-x-[0%] rounded-tl-xl rounded-bl-xl'}`}>
                     <h2 className='text-white text-xl sm:text-2xl md:text-3xl font-bold'>{isSignedUp ? 'Hello Friend!' : 'Welcome Back!'}</h2>
                     <p className='text-gray-100 max-w-[260px] mt-1 text-center leading-relaxed'>{isSignedUp ? 'Enter your personal details and start your journey with us' : 'To keep connected with us please log in with your personal info'}</p>
                     {isSignedUp 
